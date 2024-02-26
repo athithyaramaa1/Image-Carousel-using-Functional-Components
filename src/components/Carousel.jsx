1f629b183fa28c0fd8e4658162fd61e3fc225b04
athithyaramaa1 <athithya.ramma@kalvium.community>
2024-02-26 07:56:14 +0530

    Img Carousel using Fn Components

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,12 +2,35 @@ import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
 
-// you can research - how to use material ui
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
+
 function Carousel() {
+    let [index, setIndex] = useState(0)
+
+    const backPic = () => {
+        if(index === 0){
+            setIndex(2)
+        }else{
+            setIndex((prev) => prev - 1)
+        }
+    }
+    const ForwardPic = () => {
+        if(index === 2){
+            setIndex(0)
+        }else{
+            setIndex((prev) => prev + 1)
+        }
+    }
+
+    return(
+        <div className="Images-Conatainer">
+            <ArrowBackIosIcon onClick={backPic} className="backArrow" />
+            <img src={images[index].img} alt={`carousel-image-${index}`} />
+            <ArrowForwardIosIcon onClick={ForwardPic} className="frontArrow"/>
+        </div>
+    )
 }
 
 export default Carousel;
